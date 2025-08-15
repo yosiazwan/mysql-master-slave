6109a0e748be02c572877a352fa085f4be4fc7bd
Yosi Azwan <yosi.azwan@gmail.com>
2025-08-15 16:24:08 +0700

    slave status is exist, but still error

---
+++ init/slave/init-replication.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+# Load env
+REPL_USER=${REPL_USER:-repl_prod_1}
+REPL_PASSWORD=${REPL_PASSWORD:-R3plC0mpl3x!2025}
+ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-SuperRootPass!2025}
+
+# Wait master ready
+until mysql --connect-timeout=2 -h mysql-master -uroot -p"$ROOT_PASSWORD" -e "SELECT 1;" &>/dev/null; do
+  echo "[INFO] Waiting for master..."
+  sleep 2
+done
+
+# Configure replication
+mysql -uroot -p"$ROOT_PASSWORD" <<EOF
+STOP REPLICA;
+CHANGE REPLICATION SOURCE TO
+  SOURCE_HOST='mysql-master',
+  SOURCE_USER='$REPL_USER',
+  SOURCE_PASSWORD='$REPL_PASSWORD',
+  MASTER_AUTO_POSITION=1;
+START REPLICA;
+EOF
+
+echo "[INFO] Replication started"
